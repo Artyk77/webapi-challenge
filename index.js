64f59254c9c55830cb756ca06db9bf2aad9c8089
jose restrepo <arturo.restrepo@gmail.com>
2019-09-06 10:36:16 -0500

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 Sing along:
 
-here's a little code I wrote, you might want to read it really slow, don't worry be happy
+There's a little code I wrote, you might want to read it really slow, don't worry be happy
 in every line there may be trouble, but if you worry you make it double, don't worry, be happy
 ain't got no sense of what is REST? just concentrate on learning Express, don't worry, be happy
 your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
