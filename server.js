38cb9c8a0817cc724a13685b0607fda6325c54b3
jose restrepo <arturo.restrepo@gmail.com>
2019-09-06 15:58:19 -0500

    MVP

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const projectRouter = require("./projects/projectRouter.js");
+
+const server = express();
+
+server.use(express.json());
+server.use("/projects", projectRouter);
+
+
+server.get("/", (req, res) => {
+    res.status(200).send(`<h2>It is working!!!</h2>`);
+});
+module.exports = server;
